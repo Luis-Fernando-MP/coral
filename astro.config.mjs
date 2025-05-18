6e1ff69f54d1493c46c92c9376431f7fe3769747
Juls <luigfmp@gmail.com>
2025-05-18 18:56:52 -0500

    refactor: :fire: update output configuration to static and remove unused node adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,12 @@
-// @ts-check
 import { defineConfig } from 'astro/config'
 import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
 import { imageService } from '@unpic/astro/service'
-import node from '@astrojs/node'
 
-// https://astro.build/config
 export default defineConfig({
 	site: 'https://coral.vercel.app',
 	integrations: [mdx(), sitemap()],
-	output: 'server',
-	adapter: node({
-		mode: 'standalone'
-	}),
+	output: 'static',
 	image: {
 		service: imageService({
 			placeholder: 'blurhash'

853554d150beb50965291187dfac6869d7c261e5
Juls <luigfmp@gmail.com>
2025-05-18 20:02:04 -0500

    feat: :sparkles: update publication dates to 2025 and implement transition animations in Post and PostHero components

---
+++ src/lib/animation.ts
@@ -0,0 +1,18 @@
+const anim = {
+	old: {
+		name: 'bump',
+		duration: '0.5s',
+		easing: 'ease-in',
+		direction: 'reverse'
+	},
+	new: {
+		name: 'bump',
+		duration: '0.5s',
+		easing: 'ease-in-out'
+	}
+}
+
+export const tAnimation = {
+	forwards: anim,
+	backwards: anim
+}

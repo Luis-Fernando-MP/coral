75ac506b86349adbb42bb2cdde0adb03c1d78713
Juls <luigfmp@gmail.com>
2025-05-18 19:15:57 -0500

    refactor: :fire: Replace '@unpic/astro' Image component with 'astro:assets' and remove unused properties

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,10 @@ import { defineConfig } from 'astro/config'
 import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
 import vercel from '@astrojs/vercel'
-import { imageService } from '@unpic/astro/service'
 
 export default defineConfig({
 	site: 'https://coral.vercel.app',
 	integrations: [mdx(), sitemap()],
 	output: 'server',
-	adapter: vercel(),
-	image: {
-		service: imageService({
-			placeholder: 'blurhash'
-		})
-	}
+	adapter: vercel()
 })

8e243eceb2e5fea24e925bf0dbd9b8c3ad9da108
Juls <luigfmp@gmail.com>
2025-05-18 19:06:20 -0500

    ci: :bug: Refactor code structure for improved readability and maintainability

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from 'astro/config'
 import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
+import vercel from '@astrojs/vercel'
 import { imageService } from '@unpic/astro/service'
 
 export default defineConfig({
 	site: 'https://coral.vercel.app',
 	integrations: [mdx(), sitemap()],
-	output: 'static',
+	output: 'server',
+	adapter: vercel(),
 	image: {
 		service: imageService({
 			placeholder: 'blurhash'

e48385e9419d10e8e1ae0e97f4aef25c02a56e21
Juls <luigfmp@gmail.com>
2025-05-18 19:19:49 -0500

    refactor: :bug: Refactor code structure for improved readability and maintainability

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 import { defineConfig } from 'astro/config'
 import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
-import vercel from '@astrojs/vercel'
 
 export default defineConfig({
 	site: 'https://coral.vercel.app',
 	integrations: [mdx(), sitemap()],
-	output: 'server',
-	adapter: vercel()
+	output: 'static'
 })
